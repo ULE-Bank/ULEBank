906566ea53d341cc3ca409c6906a7d07eb2d607e
Razvan Raducu <rraduc00@estudiantes.unileon.es>
2017-05-16 19:02:31 +0200

    Instalado y desinstalado EvoSuite. Implementando Tests.

--- ulebankoffice/src/test/java/es/unileon/ulebankoffice/domain/SolicitudFinancialAdvisorDomainTest.java
+++ ulebankoffice/src/test/java/es/unileon/ulebankoffice/domain/SolicitudFinancialAdvisorDomainTest.java
@@ -1,19 +1,44 @@
 package es.unileon.ulebankoffice.domain;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class SolicitudFinancialAdvisorDomainTest {
-
+	
+	private SolicitudFinancialAdvisorDomain solicitud;
+	
 	@Before
 	public void setUp() throws Exception {
+		solicitud = new SolicitudFinancialAdvisorDomain();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTextoOferta() {
+		assertNull(solicitud.getTextoOferta());
+		solicitud.setTextoOferta("texto");
+		assertThat(solicitud.getTextoOferta(), is("texto"));
+	}
+
+	@Test
+	public void testEnlaceOferta() {
+		assertNull(solicitud.getEnlaceOferta());
+		solicitud.setTextoOferta("texto");
+		assertThat(solicitud.getTextoOferta(), is("texto"));
+	}
+
+	@Test
+	public void testRutaOferta() {
+		assertNull(solicitud.getRutaOferta());
+		solicitud.setRutaOferta("ruta");
+		assertThat(solicitud.getRutaOferta(), is("ruta"));
+	}
+
+	@Test
+	public void testGetIdSolicitud() {
+		assertNull(solicitud.getIdSolicitud());
 	}
 
 }
