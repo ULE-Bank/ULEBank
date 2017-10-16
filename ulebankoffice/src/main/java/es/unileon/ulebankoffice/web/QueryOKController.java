982870400b39e80dd90ce10cd225f4f19c73cb9b
Razvan Raducu <rraduc00@estudiantes.unileon.es>
2017-10-16 18:05:13 +0200

    Empezando a hacer comprobaciones del resultado del TEST

--- ulebankoffice/src/main/java/es/unileon/ulebankoffice/web/QueryOKController.java
+++ ulebankoffice/src/main/java/es/unileon/ulebankoffice/web/QueryOKController.java
@@ -3,20 +3,40 @@
  */
 package es.unileon.ulebankoffice.web;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.unileon.ulebankoffice.domain.AdvisorUserDomain;
+import es.unileon.ulebankoffice.repository.AdvisorUserRepository;
 
 /**
  * @author Razvan Raducu
  *
  */
 @Controller
-@RequestMapping(value="/offersconsutting/queryok")
+@RequestMapping(value="/offersconsulting/queryok")
 public class QueryOKController {
+	
+	@Autowired
+	private AdvisorUserRepository repo;
 
 	@GetMapping
-	public String controllerVacio(){
+	public String controllerVacio(ModelMap model, 	Principal principal){
+		
+		String emailUsuario = principal.getName();
+		
+		AdvisorUserDomain user = repo.findByEmail(emailUsuario);
+		
+		if(user.getResultadoTest() <= 0.0){
+			model.addAttribute("testPendiente", 1);
+			model.addAttribute("emailUsuario", emailUsuario);
+		}
+		
 		return "question-verification";
 	}
 	
